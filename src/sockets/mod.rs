f95bb1c92042ae6d1f2d45c769447dfb7af9f712
Joseph Murphy <air.jmurph@gmail.com>
2021-01-19 19:15:03 -0600

    Fixed feature flagging for zmq-server and got it building

--- src/sockets/mod.rs
+++ src/sockets/mod.rs
@@ -1,7 +1,15 @@
-mod client;
-mod controller;
-mod server;
 
+#[cfg(feature = "zmq-client")]
+mod client;
+#[cfg(feature = "zmq-client")]
 pub use client::*;
+
+#[cfg(feature = "zmq-server")]
+mod controller;
+#[cfg(feature = "zmq-server")]
 pub use controller::*;
+
+#[cfg(feature = "zmq-server")]
+mod server;
+#[cfg(feature = "zmq-server")]
 pub use server::*;
--- src/sockets/server.rs
+++ src/sockets/server.rs
@@ -1,8 +1,11 @@
+use async_trait::async_trait;
 use super::CandelaSocketController;
 use crate::{types, CandelaServer, CandelaServerConfig, Result};
 
+#[derive(Debug)]
 pub struct CandelaSocketServer {}
 
+#[async_trait]
 impl CandelaServer for CandelaSocketServer {
     type Controller = CandelaSocketController;
 
@@ -10,13 +13,15 @@ impl CandelaServer for CandelaSocketServer {
         unimplemented!()
     }
 
-    fn search() -> Vec<types::LedControllerConfig> {
+    async fn search() -> Vec<types::LedControllerConfig> {
         unimplemented!()
     }
+
+    async fn connect(_config: types::LedControllerConfig) -> Result<()> {
+        unimplemented!()
+    }
+
     fn get_controllers(&mut self) -> &mut Vec<Self::Controller> {
-        unimplemented!()
-    }
-    fn connect(_config: types::LedControllerConfig) -> Result<()> {
         unimplemented!()
     }
 }
